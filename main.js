139e0c8205f733cd4e574f28708b10cb53a8a576
awesome110 <83648588+awesome110@users.noreply.github.com>
2022-05-07 16:21:22 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,1 @@
-let myLibrary = [
-  {
-    title:bookTitle,
-    author:bookAuthor,
-    number_of_pages:bookPages,
-    read:false;
-  }
-];
 
-function Book() {
-  
-}
-
-function addBookToLibrary() {
-  
-}
